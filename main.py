b406920aa4648b00dc951836abe473b1d85df61d
niblicat <maybenaebli@gmail.com>
2024-01-24 21:13:13 -0600

    standard group making function implemented

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from openpyxl import Workbook
 import csv
+from typing import List
 
 def CreateXLSXFromCSV(name = 'input.csv'):
     wb = Workbook()
@@ -17,9 +18,28 @@ def CreateXLSXFromCSV(name = 'input.csv'):
 def SortXLSX(name = "group_make.xlsx"):
     myFile = pd.read_excel(f"{name}", engine="openpyxl")
 
-    myFile.sort_values(by=myFile.columns[7], inplace=True, ascending=False)
+    myFile.sort_values(by=myFile.columns[7], inplace=True, ascending=True)
     myFile.reset_index(drop=True, inplace=True)
     return myFile
+
+def MakeGroups(students: pd.DataFrame, min: int):
+    total = students.__len__()
+    numGroups = total // min
+
+    groups = [pd.DataFrame(columns=['name', 'score']) for _ in range(numGroups)]
+    
+    for i in range(total):
+        group_index = i % numGroups
+        row = pd.DataFrame({'name': [students.at[i, 'name']], 'score': [students.at[i, 'score']]})
+        groups[group_index] = pd.concat([groups[group_index], row], ignore_index=True)
+
+    # Concatenate the list of DataFrames into one DataFrame
+    # result = pd.concat(groups, ignore_index=True)
+    print(groups)
+    # print("num groups",numGroups)
+    # print(result)
+    return groups
+
 
 def main():
     inputFileName = input("file name: ")
@@ -28,10 +48,14 @@ def main():
     myFile = SortXLSX()
 
     scores = myFile.iloc[:, 7]
-    fullName = myFile.iloc[:, 0] + ', ' + myFile.iloc[:, 1]
+    names = myFile.iloc[:, 0] + ', ' + myFile.iloc[:, 1]
 
-    print(fullName)
-    print(scores)
+    # print(names)
+    # print(scores)
+    students = pd.DataFrame({'name': names, 'score': scores})
+    print(students)
+
+    groups = MakeGroups(students, 4)
 
 if __name__ == "__main__":
     main()
